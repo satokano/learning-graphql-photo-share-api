20aa71635357ebda45e736e007875044c18b9506
Satoshi Okano <satokano@gmail.com>
2020-01-04 20:39:33 +0900

    use mongodb

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const { readFileSync } = require(`fs`)
 
 const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`)
 
+const { MongoClient } = require(`mongodb`)
+require(`dotenv`).config()
+
 // ユニークID
 var _id = 0
 
@@ -50,61 +53,77 @@ var tags = [
     { "photoID": "2", "userID": "gPlake" }
 ]
 
-const  resolvers = {
-    Query: {
-        // 写真を格納した配列の長さを返す
-        totalPhotos: () => photos.length,
-        allPhotos: () => photos
-    },
-    Mutation: {
-        // postPhotoミューテーションと対応するリゾルバ
-        postPhoto(parent, args) {
-            var newPhoto = {
-                id: _id++,
-                ...args.input,
-                created: new Date()
+async function start() {
+    const app = express()
+    const MONGO_DB = process.env.DB_HOST
+    let db
+
+    const client = await MongoClient.connect(
+        MONGO_DB,
+        { useNewUrlParser: true }
+    )
+    db = client.db()
+    const context = { db }
+
+    const  resolvers = {
+        Query: {
+            totalPhotos: (parent, args, { db }) => 
+                db.collection('photos').estimatedDocumentCount(),
+            allPhotos: (parent, args, { db }) =>
+                db.collection('photos').find().toArray(),
+            totalUsers: (parent, args, { db }) =>
+                db.collection('users').estimatedDocumentCount(),
+            allUsers: (parent, args, { db }) =>
+                db.collection('users').find().toArray()
+        },
+        Mutation: {
+            // postPhotoミューテーションと対応するリゾルバ
+            postPhoto(parent, args) {
+                var newPhoto = {
+                    id: _id++,
+                    ...args.input,
+                    created: new Date()
+                }
+                photos.push(newPhoto)
+                return newPhoto
             }
-            photos.push(newPhoto)
-            return newPhoto
-        }
-    },
-    Photo: {
-        url: parent => `http://localhost:4040/img/${parent.id}.jpg`,
-        postedBy: parent => {
-            return users.find(u => u.githubLogin === parent.githubUser)
         },
-        taggedUsers: parent => tags
-            .filter(tag => tag.photoID === parent.id)
-            .map(tag => tag.userID)
-            .map(userID => users.find(u => u.githubLogin === userID))
-    },
-    User: {
-        postedPhotos: parent => {
-            return photos.filter(p => p.githubUser === parent.githubLogin)
+        Photo: {
+            url: parent => `http://localhost:4040/img/${parent.id}.jpg`,
+            postedBy: parent => {
+                return users.find(u => u.githubLogin === parent.githubUser)
+            },
+            taggedUsers: parent => tags
+                .filter(tag => tag.photoID === parent.id)
+                .map(tag => tag.userID)
+                .map(userID => users.find(u => u.githubLogin === userID))
         },
-        inPhotos: parent => tags
-            .filter(tag => tag.userID === parent.id)
-            .map(tag => tag.photoID)
-            .map(photoID => photos.find(p => p.id === photoID))
-    },
-    DateTime: new GraphQLScalarType({
-        name: `DateTime`,
-        description: `A valid date time value.`,
-        parseValue: value => new Date(value),
-        serialize: value => new Date(value).toISOString(),
-        parseLiteral: ast => ast.value
-    })
+        User: {
+            postedPhotos: parent => {
+                return photos.filter(p => p.githubUser === parent.githubLogin)
+            },
+            inPhotos: parent => tags
+                .filter(tag => tag.userID === parent.id)
+                .map(tag => tag.photoID)
+                .map(photoID => photos.find(p => p.id === photoID))
+        },
+        DateTime: new GraphQLScalarType({
+            name: `DateTime`,
+            description: `A valid date time value.`,
+            parseValue: value => new Date(value),
+            serialize: value => new Date(value).toISOString(),
+            parseLiteral: ast => ast.value
+        })
+    }
+
+
+    const server = new ApolloServer({ typeDefs, resolvers, context })
+    server.applyMiddleware({ app })
+    app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`))
+    app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }))
+    app.listen({ port: 4000 }, () => 
+        console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
+    )
 }
 
-
-var app = express()
-const server = new ApolloServer({ typeDefs, resolvers })
-
-server.applyMiddleware({ app })
-
-app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`))
-app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }))
-
-app.listen({ port: 4000 }, () => 
-    console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-)
+start()
